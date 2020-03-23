4b79c144367518fda2c9bdcfc8cabdb517458524
kenny-ni <jiageng.ni@outlook.com>
2020-03-23 11:43:36 -0700

    finished

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# boogle-solution
-a solution to the boogle problem(finding all valid words on a board) using DFS and trie data structure
+# boggle-solution
+A solution to the boogle problem(finding all valid words on a board) using DFS and trie data structure
+
+ps: 
+1. nltk package is used in this solution to build the dictionary, please use `pip install nltk` to install the package.
+2. after installing the nltk package, perform the following command to download module needed:   
+`import nltk`    
+`nltk.download('words')`
+
--- code_challenge.py
+++ code_challenge.py
@@ -1,12 +1,15 @@
-#%%
 class TrieNode:
+    """
+    class for a node in the trie
+    """
     def __init__(self):
         self.isWord = False
         self.next_char = {}
 
 def build_helper (remaining, node):
     """
-    used to help build the trie
+    used to help build the trie recursively
+
     param: 
     remaining: string (remaining part of the word)
     node: TrieNode (current node of the trie)
@@ -15,10 +18,9 @@ def build_helper (remaining, node):
     none
     """
     if len(remaining) == 0: #exit of recursion
-        return 
-
-    if len(remaining) == 1:
         node.isWord = True
+        return 
+        
     if remaining[0] not in node.next_char:
         node.next_char[remaining[0]] = TrieNode()
     node = node.next_char[remaining[0]]
@@ -67,7 +69,7 @@ def get_neighbors(row, col, board):
 
 def dfs (row, col, curr_word, visited, results, board, trie):
     """
-    used to perform dfs search on the given board
+    used to perform dfs search on the given board from current location
 
     param: 
     row: int (row coordinate of the current char to visit)
@@ -98,29 +100,50 @@ def dfs (row, col, curr_word, visited, results, board, trie):
     curr_word.pop()
 
 def find_words(board, words):
+    """
+    solution to the challenge, will print each found words and return the set of all found words
+
+    param: 
+    board: list of list (the char board)
+    words: list (all valid words)
+
+    return: set (all found words)
+    """
+    #convert all char to lower case firstly
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            board[row][col] = board[row][col].lower()
     results = set()
     visited = set()
     trie_root = build_trie(words)
-    dfs(0, 0, [], visited, results, board, trie_root)
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            dfs(row, col, [], visited, results, board, trie_root)
     print('words found:')
     for word in results:
         print(word)
+    print(f'total number of words found: {len(results)}')
+    return results
 
 
-#%%
-board = [['c', 'n', 't', 's', 's'],
-        ['d', 'a', 't', 'i', 'n'],
-        ['o', 'o', 'm', 'e', 'l'],
-        ['s', 'i', 'k', 'n', 'd'],
-        ['p', 'i', 'c', 'l', 'e']]
+if __name__ == "__main__":
+    #use nltk package to get all possible words
+    from nltk.corpus import words 
+    eng_words = words.words()
 
-words = ['apple', 'pickle', 'side', 'kick', 'sick', 'mood', 'cat',
-                 'cats', 'man', 'super', 'antman', 'godzilla', 'dog', 'dot',
-                 'sine', 'cos', 'signal', 'bitcoin', 'cool', 'zapper']
+    #initialize the board
+    board1 = [['c', 'n', 't', 's', 's'],
+              ['d', 'a', 't', 'i', 'n'],
+              ['o', 'o', 'm', 'e', 'l'],
+              ['s', 'i', 'k', 'n', 'd'],
+              ['p', 'i', 'c', 'l', 'e']]
 
+    board2 = [['R', 'A', 'E', 'L'],
+              ['M', 'O', 'F', 'S'],
+              ['T', 'E', 'O', 'K'],
+              ['N', 'A', 'T', 'I']]
 
+    find_words(board1, eng_words)
+    print('\n-----------------\n')
+    find_words(board2, eng_words)
 
-# %%
-find_words(board, words)
-
-# %%
